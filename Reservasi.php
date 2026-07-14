01a91abd0b532f6b993834003c433dc619205894
gilangrinakitt-jpg <gilangrinakitt@gmail.com>
2026-07-14 09:13:14 +0700

    tahap 3

---
+++ Reservasi.php
@@ -0,0 +1,56 @@
+<?php
+
+abstract class Reservasi
+{
+    protected $idReservasi;
+    protected $namaPelanggan;
+    protected $tanggalBooking;
+    protected $durasiJam;
+    protected $tarifDasarPerJam;
+
+    public function __construct(
+        $idReservasi,
+        $namaPelanggan,
+        $tanggalBooking,
+        $durasiJam,
+        $tarifDasarPerJam
+    ) {
+        $this->idReservasi = $idReservasi;
+        $this->namaPelanggan = $namaPelanggan;
+        $this->tanggalBooking = $tanggalBooking;
+        $this->durasiJam = $durasiJam;
+        $this->tarifDasarPerJam = $tarifDasarPerJam;
+    }
+
+    public function getIdReservasi()
+    {
+        return $this->idReservasi;
+    }
+
+    public function getNamaPelanggan()
+    {
+        return $this->namaPelanggan;
+    }
+
+    public function getTanggalBooking()
+    {
+        return $this->tanggalBooking;
+    }
+
+    public function getDurasiJam()
+    {
+        return $this->durasiJam;
+    }
+
+    public function getTarifDasarPerJam()
+    {
+        return $this->tarifDasarPerJam;
+    }
+
+    // Method abstract yang wajib diimplementasikan
+    abstract public function hitungTotalBiaya();
+
+    abstract public function tampilDetailReservasi();
+}
+
+?>
